ce5926b6a4fe01588d85e5cf97ae0beef3320a8d
Dimitris Michalis <dimitris.j.michalis@gmail.com>
2023-02-28 13:11:04 +0200

    Delete unnecessary libraries

--- necessary_functions/class_shop.py
+++ necessary_functions/class_shop.py
@@ -4,7 +4,6 @@ import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from check_times import check_if_and_times
 
