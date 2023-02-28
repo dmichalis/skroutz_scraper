99eb52d9d9e47b2aef2f5281f75d3a1e6fe3d827
Dimitris Michalis <dimitris.j.michalis@gmail.com>
2023-02-28 12:54:23 +0200

    Update class_shop.py

--- necessary_functions/class_shop.py
+++ necessary_functions/class_shop.py
@@ -3,7 +3,6 @@ import requests
 import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
